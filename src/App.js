45aad814c6fff79e6412882055e5b8337ab13e30
Uriel Sánchez <17171295@itculiacan.edu.mx>
2020-05-15 01:46:13 -0600

    search function, undefined validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 const api = {
 	key: '2bfeaabdca00f9f2e594409eff15fb98',
 	base: 'http://api.openweathermap.org/data/2.5/',
 };
 
 function App() {
+	const [query, setQuery] = useState('');
+	const [weather, setWeather] = useState({});
+
+	const search = (evt) => {
+		if (evt.key === 'Enter') {
+			fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+				.then((res) => res.json())
+				.then((result) => {
+					setWeather(result);
+					setQuery('');
+				});
+		}
+	};
+
 	const dateBuilder = (d) => {
 		let months = [
 			'January',
@@ -31,21 +45,38 @@ function App() {
 	};
 
 	return (
-		<div className="app cold">
+		<div
+			className={
+				typeof weather.main != 'undefined' ? (weather.main.temp > 20 ? 'app warm' : 'app') : 'app'
+			}
+		>
 			<main>
 				<div className="search-box">
-					<input type="text" className="search-bar" placeholder="Search..." />
+					<input
+						type="text"
+						className="search-bar"
+						placeholder="Search..."
+						onChange={(e) => setQuery(e.target.value)}
+						value={query}
+						onKeyPress={search}
+					/>
 				</div>
-				<div>
-					<div className="location-box">
-						<div className="location">Culiacan, Sinaloa, MX</div>
-						<div className="date">{dateBuilder(new Date())}</div>
+				{typeof weather.main != 'undefined' ? (
+					<div>
+						<div className="location-box">
+							<div className="location">
+								{weather.name}, {weather.sys.country}
+							</div>
+							<div className="date">{dateBuilder(new Date())}</div>
+						</div>
+						<div className="weather-box">
+							<div className="temp">{Math.round(weather.main.temp)}°C</div>
+							<div className="weather">{weather.weather[0].main}</div>
+						</div>
 					</div>
-					<div className="weather-box">
-						<div className="temp">15°C</div>
-						<div className="weather">Sunny</div>
-					</div>
-				</div>
+				) : (
+					''
+				)}
 			</main>
 		</div>
 	);
