ba7aa83f504323253288aedbfcad5d7a00b12a60
Uriel Sánchez <17171295@itculiacan.edu.mx>
2020-05-15 01:12:46 -0600

    dateBuilder function

--- src/App.js
+++ src/App.js
@@ -5,7 +5,50 @@ const api = {
 };
 
 function App() {
-	return <div className="App"></div>;
+	const dateBuilder = (d) => {
+		let months = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December',
+		];
+		let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+		let day = days[d.getDay()];
+		let date = d.getDate();
+		let month = months[d.getMonth()];
+		let year = d.getFullYear();
+
+		return `${day} ${date} ${month} ${year}`;
+	};
+
+	return (
+		<div className="app cold">
+			<main>
+				<div className="search-box">
+					<input type="text" className="search-bar" placeholder="Search..." />
+				</div>
+				<div>
+					<div className="location-box">
+						<div className="location">Culiacan, Sinaloa, MX</div>
+						<div className="date">{dateBuilder(new Date())}</div>
+					</div>
+					<div className="weather-box">
+						<div className="temp">15°C</div>
+						<div className="weather">Sunny</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
