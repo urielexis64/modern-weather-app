3020122bd3ceb3cb236c676b8f08cc25eae75798
Uriel Sánchez <17171295@itculiacan.edu.mx>
2020-05-15 01:54:08 -0600

    Updates

---
+++ precache-manifest.1f5d302b92dfc4d792e44004b87bfc7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78db2caf5c1792bfec9f4bd396fee2ac",
+    "url": "/modern-weather-app/index.html"
+  },
+  {
+    "revision": "bf95c68a16732cf21545",
+    "url": "/modern-weather-app/static/css/main.eda22968.chunk.css"
+  },
+  {
+    "revision": "e94eea44173db580e8fe",
+    "url": "/modern-weather-app/static/js/2.0b6eca92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/modern-weather-app/static/js/2.0b6eca92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf95c68a16732cf21545",
+    "url": "/modern-weather-app/static/js/main.19b8f9f8.chunk.js"
+  },
+  {
+    "revision": "4855a2664f59cc5c9bd5",
+    "url": "/modern-weather-app/static/js/runtime-main.f3988624.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/modern-weather-app/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/modern-weather-app/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
