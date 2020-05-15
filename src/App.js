9e17daa77ed244307330790b6ebcc410844af039
Uriel Sánchez <17171295@itculiacan.edu.mx>
2020-05-14 23:55:04 -0600

    Unused files removed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const api = {
+	key: '2bfeaabdca00f9f2e594409eff15fb98',
+	base: 'http://api.openweathermap.org/data/2.5/',
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <div className="App"></div>;
 }
 
 export default App;
